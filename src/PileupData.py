354732d9b7c43722598214dcadd93bb854951362
indapa <indapa@gmail.com>
2012-10-22 17:46:52 -0400

    added getSamplePileupData function

--- src/PileupData.py
+++ src/PileupData.py
@@ -1,20 +1,22 @@
+from itertools import groupby
 class PileupData(object):
     """ represents a pileup column of reads for a chromosome and posiiton
         please note, start/stop is zero based, half open interval consistent
         with bed file"""
 
 
-    def __init__(self, chrom, start, end, pileuplist=[]):
+    def __init__(self, chrom, start, end, pos, pileuplist=[]):
         self.chrom=chrom
         self.start=start
         self.end=end
+        self.pileupcolumn_pos=pos
         """ this will be a list of tuples
             with the tuples consisting of (readgroup,sample,alignment name, base, base quality"""
         self.pileupList=pileuplist
 
-    def __str__:
-        return "\n".join( self.chrom, str(self.start), str(self.end) )
-
+   
+    def __str__(self):
+        return "\t".join( [self.chrom, str(self.start), str(self.end), str(self.pileupcolumn_pos)])
 
     def getChrom(self):
         return self.chrom
@@ -31,6 +33,10 @@ class PileupData(object):
     def setEnd(self,end):
         self.end=end
 
+    def getCoverage(self):
+        """ the coverage is the total number of records  in the piluepcolumn_pos list """
+        return len( self.pileupcolumn_pos)
+
     def yieldSamplePileupData(self):
         """ we can yield a list of pileup data by grouping info by sample
             see http://docs.python.org/release/3.2.3/library/itertools.html#itertools.groupby
@@ -40,4 +46,9 @@ class PileupData(object):
         for key, group in groupby(self.pileupList, lambda x: x[1]):
             yield list(group)
 
+    def getSamplePileupData(self, samplename):
+        """ return the pileupdata for the particular samplename   """
+        return [ tple for tple in self.pileupList if tple[0]== samplename ]
+
+    
 
