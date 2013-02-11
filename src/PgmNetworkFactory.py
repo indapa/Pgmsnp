b98ca97db412bdfc99482dfd9cdc05ae4f8b6e40
indapa <indapa@gmail.com>
2013-02-11 18:09:42 -0500

    fixed some function to re-arrange parameter lists for functions

---
+++ src/PgmNetworkFactory.py
@@ -0,0 +1,96 @@
+from Factor import *
+from FactorOperations import *
+from PedigreeFactors import *
+from PgmsnpCommon import *
+#import itertools
+#import numpy as np
+"""" Still not sure how this is going to work
+     This class is a factory for generating a genetic network
+     If we consider each location in the genome independent
+     we generate a new network for each position along an interval.
+     A network is a collection of factors, so we return a python
+     list of Factor objects. Each (read) phenotype is conditionally
+     independent given its genotype, so each member of the pedigree has a
+     PhenotypeGivenGenotypeFactor. If a member is a founder it is represented by
+     a returnGenotypePriorFounderFactor. If a member is a non-founder it is reprsented by
+     a returnGenotypeGivenParentsFactor. The first is a simple factor representing a genotype prior.
+     The second is basically a Punnet square representing Mendelian probabilities.
+     """
+
+class PgmNetworkFactory(object):
+
+    """ We construct our network based on a pedigree file, chromosome, and posiiton.
+        The number of alleles is the number of possible alleles segregating at a site.
+        For the full probablistic calculation it set at 4 (ACGT). In practice, assuming
+        most sites are bi-allelic its value is 2."""
+
+    def __init__(self, pedfile, chrom, position, refbase, totalAlleles=4):
+        #parse pedfile
+        
+        
+        self.chrom=chrom
+        self.pos=position
+        self.refbase=refbase
+        self.totalAlleles=totalAlleles
+        self.pedigree=Pedfile(pedfile)
+        self.pedigree.parsePedfile()
+        self.pedlist=self.pedigree.getPedList()
+        self.pedids=self.pedigree.returnIndivids()
+        
+
+        #list of factors that will comprise the Genetic network
+        self.totalFactors=self.pedigree.getTotalSize() * 2
+        self.factorList=self.totalFactors*[None]
+
+        self.constructNetwork()
+
+
+
+    def constructNetwork(self):
+        """ make a new network  """
+        totalPeople=self.pedigree.getTotalSize()
+        for i in range( totalPeople ):
+            
+            
+            if self.pedlist[i].isFounder():
+                """ if its a founder invidual, assign it GenotypePriorFounderFactor """
+                self.factorList[i]=returnGenotypePriorFounderFactor( self.refbase,i )
+
+                #previously, we assigned it a factor based on HWE and given allele frequencies.
+                #I suppose we could use this for genotyping previoulsy known sites
+
+                #self.factorList[i]=GenotypeAlleleFreqFactor(self.allelefreq,self.pedlist[i].getid(),self.pedlist[i].getid())
+                #factorList(i)=genotypeGivenAlleleFreqsFactor(alleleFreqs,i);
+            else:
+                """ for non-founder we make GenotypeGivenParentsFactor """
+                #3print self.pedlist[i].getParents(), self.pedlist[i].getid()
+                #GenotypeGivenParentsFactor(2,"bart","homer","marge","""Bart | Homer, Marge """)
+                #self.factorList[i]=GenotypeGivenParentsFactor(self.totalAlleles, self.pedlist[i].getid(), self.pedlist[i].getParents()[0], self.pedlist[i].getParents()[1], "child|Father,Child")
+                parent1Index=self.pedids.index( self.pedlist[i].getParents()[0] )
+                parent2Index=self.pedids.index( self.pedlist[i].getParents()[1] )
+                child=self.pedlist[i].getid()
+                print 'child: ', type(child)
+                parent1name=self.pedlist[parent1Index].getid()
+                parent2name=self.pedlist[parent2Index].getid()
+                print 'p1: ', type(parent1name)
+                print 'p2: ', type(parent2name)
+                name= " ".join ( [child+" genotype | ", "parent " + parent1name, ",",  "parent " + parent2name] )
+                
+                #returnGenotypeGivenParentsFactor(  genotypeVarChild, genotypeVarParentOne, genotypeVarParentTwo, numAlleles=4  )
+                self.factorList[i]=returnGenotypeGivenParentsFactor(i, parent1Index ,  parent2Index , name, self.totalAlleles)
+                
+                #returnGenotypeGivenParentsFactor(numAlleles, genotypeVarChild, genotypeVarParentOne, genotypeVarParentTwo)
+
+            name=self.pedlist[i].getid()+" read phenotype | " + self.pedlist[i].getid() + " genotype"
+            #GLFactor = Factor( [readVar, genoVar], [1,10], [], 'read_phenotype | genotype ')
+            self.factorList[i+totalPeople]=Factor([i+totalPeople,i],[],[], name )
+
+    def getFactorList(self):
+        return self.factorList
+
+    def printFactorList(self):
+        for f in self.factorList:
+            print "factor: ", f
+        #return "\n".join(networkOutstrings)
+
+
