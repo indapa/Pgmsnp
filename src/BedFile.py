ecc20712559e01b00f308aed773166ae1cbe8350
indapa <indapa@gmail.com>
2013-03-21 17:38:14 -0400

    re-commit

--- src/BedFile.py
+++ src/BedFile.py
@@ -2,21 +2,21 @@ import sys
 class Bedfile(object):
     """ represent a bedfile """
 
-def __init__(self, bedfilename):
-    self.name=bedfilename
-    self.fh=None
+    def __init__(self, bedfilename):
+        self.name=bedfilename
+        self.fh=None
 
-def open(self):
-    try:
-        self.fh=open(self.name, 'r')
+    def open(self):
+        try:
+            self.fh=open(self.name, 'r')
 
-    except IOError:
-        sys.stderr.write("Error: cannot open bedfile for reading.\n")
+        except IOError:
+            sys.stderr.write("Error: cannot open bedfile for reading.\n")
 
-def yield_bedcoordinate(fh):
-    """ yield a tuple of (chr, start,end) from bed file """
-    for line  in self.fh:
-        if '@' in line: continue
-        fields=line.strip().split("\t")
-        (chr, start, end) = fields[0:3]
-        yield(chr, int(start), int(end) )
+    def yield_bedcoordinate(self):
+        """ yield a tuple of (chr, start,end) from bed file """
+        for line  in self.fh:
+            if '@' in line: continue
+            fields=line.strip().split("\t")
+            (chr, start, end) = fields[0:3]
+            yield(chr, int(start), int(end) )
