d4b3b2cecb10bab5d35121fa2acf4eb6f9a31c9e
Amit Indap <indapa@gmail.com>
2013-08-26 12:21:22 -0400

    function yieldPileupData gets passed in min map quality and min base quality default is 0. If the the mapq and bq are below the threshold the read is skipped and not anlayzed

--- src/PileupFactory.py
+++ src/PileupFactory.py
@@ -1,6 +1,6 @@
 from PileupData import PileupData
 #from BedFile import Bedfile
-
+import pdb
 class PileupFactory(object):
     """ generates PileupData objects given a Pysam and bed file object """
 
@@ -20,33 +20,42 @@ class PileupFactory(object):
 
 
 
-    def yieldPileupData(self):
+    def yieldPileupData(self, minmapq=0, minbq=0):
         """ iterate thru the bedobj bed intervals
             processing the pileupcolumns
             for each of the reads in the pileupcolumn get the RG, SM, basecall, and base quality"""
         #here we just iterate thru the pileup columns collecting the observed basecalls and qualities in list of tuples
 
         for (chrom, start, end) in self.bedobj.yield_bedcoordinate():
+            
 
             """ the pileup engine gets all reads that overlap the bed interval
                 if the pileupcolumn.pos is out of range of the bed interval, skip it"""
 
             pileup_iter=self.pybam.pileup( chrom,start,end,stepper = "all" )
             for pileupcolumn in pileup_iter:
-
+                
                 if pileupcolumn.pos+1 > end: continue
                 if pileupcolumn.pos < start: continue
 
                 observed_data=[] #a list of (basecall, basequality) tuples
                 for pileupread in pileupcolumn.pileups:
+                    #pdb.set_trace()
+                    if pileupread.alignment.mapq < minmapq: continue
+                    
+                    
                     if pileupread.alignment.seq[pileupread.qpos] == 'N':continue
                     bq=ord ( pileupread.alignment.qual[ pileupread.qpos ] )  - 33
-                    if bq == 0: continue
-                    #print pileupread.alignment.qname
+                    if bq < minbq: continue
+                   
+                    #if bq == 0: continue
+                    #print pileupread.alignment
                     #tid=pileupread.alignment.tid
-                    
+                   
                     readgroup=dict( pileupread.alignment.tags )['RG']
+                    
                     sample=self.readgroupdict[readgroup]
+                    #print 'mapq: ', pileupread.alignment.mapq, " bq: ", bq, pileupread.alignment.qname, sample
                     #ascii conver the basequality
                     
                     
